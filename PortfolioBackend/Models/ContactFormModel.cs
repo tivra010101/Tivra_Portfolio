4c84b63f8e43fa20a70112ddbb2a5c1039f7ffad
Tivra Pal Singh Chauhan <tivra010101@gmail.com>
2025-03-19 23:46:02 +0530

    Fixed nullable properties in ContactFormModel

--- PortfolioBackend/Models/ContactFormModel.cs
+++ PortfolioBackend/Models/ContactFormModel.cs
@@ -4,11 +4,11 @@ namespace PortfolioBackend.Models
 {
     public class ContactFormModel
     {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Subject { get; set; }
-        public string Message { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Subject { get; set; }
+        public string? Message { get; set; }
     }
 
     [JsonSerializable(typeof(ContactFormModel))]
